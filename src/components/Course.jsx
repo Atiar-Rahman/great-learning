8ceb86fa216dec2294bf565906b9f10fb5fc874b
Atiar-Rahman <atiar1709@gmail.com>
2025-04-13 07:07:33 +0600

    add the course delete functionality

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const Course = ({ course }) => {
-    console.log(course)
+    // console.log(course)
+    const handleDelete = _id => {
+        console.log(_id)
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:3000/course/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        // console.log(data)
+                        if (data.deletedCount>0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your Course has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
     return (
         <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
             <table className="table">
@@ -14,7 +44,7 @@ const Course = ({ course }) => {
                         <td>{course.instructorName}</td>
                         <td>{course.money}</td>
                         <td><Link className='btn btn-outline'>Update</Link></td>
-                        <td><Link className='btn btn-outline'>delete</Link></td>
+                        <td><Link onClick={() => handleDelete(course._id)} className='btn btn-outline'>delete</Link></td>
 
                     </tr>
                 </tbody>
